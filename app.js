70b3bb12b1a7dfe281193ceeeb3e38fb23a0ba6c
Ulv122 <qemmmberov22@gmail.com>
2025-04-30 15:10:11 +0400

    andersson2

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const buttons = document.querySelectorAll('.filter-btn');
 const items = document.querySelectorAll('.portfolio-item');
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    // Aktiv düyməni yaşıl et
-    buttons.forEach(btn => btn.classList.remove('active')); // Bütün düymələrdən 'active' sinifini silirik
-    button.classList.add('active'); // Kliklənmiş düyməyə 'active' sinifi əlavə edirik
+    buttons.forEach(btn => btn.classList.remove('active')); 
+    button.classList.add('active'); 
 
     const filter = button.textContent.toLowerCase();
     items.forEach(item => {
